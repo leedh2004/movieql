039888b7cf8d3c11924b15b233c19f91cc733254
이도현 <leedh2008@naver.com>
2020-07-27 13:25:24 +0900

    graphql-yoga  example

---
+++ graphql/db.js
@@ -0,0 +1,16 @@
+const API_URL = "https://yts.mx/api/v2/list_movies.json?"
+import fetch  from "node-fetch";
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if(limit > 0){
+        REQUEST_URL += `limit=${limit}`;
+    }
+    if(rating > 0){
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+
+    return fetch(`${REQUEST_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+};
---
+++ index.js
@@ -0,0 +1,10 @@
+//const graphqlYoga = require("graphql-yoga");
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
+
+const server = new GraphQLServer({
+    typeDefs: "graphql/schema.graphql",
+    resolvers
+})
+
+server.start(() => console.log("Graphql Server Running"));
